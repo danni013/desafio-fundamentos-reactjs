b68341db1d33a91d8cabdbb503bf0f7d6d6e6f44
Daniele Oliveira <daniele.o.rodrigues@hotmail.com>
2020-06-12 18:47:36 -0300

    Resolução do Desafio

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,14 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
+import api from '../../services/api';
 
 import income from '../../assets/income.svg';
 import outcome from '../../assets/outcome.svg';
 import total from '../../assets/total.svg';
 
-import api from '../../services/api';
-
 import Header from '../../components/Header';
-
 import formatValue from '../../utils/formatValue';
+import formatDate from '../../utils/formatDate';
 
 import { Container, CardContainer, Card, TableContainer } from './styles';
 
@@ -16,8 +15,6 @@ interface Transaction {
   id: string;
   title: string;
   value: number;
-  formattedValue: string;
-  formattedDate: string;
   type: 'income' | 'outcome';
   category: { title: string };
   created_at: Date;
@@ -30,42 +27,42 @@ interface Balance {
 }
 
 const Dashboard: React.FC = () => {
-  // const [transactions, setTransactions] = useState<Transaction[]>([]);
-  // const [balance, setBalance] = useState<Balance>({} as Balance);
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [balance, setBalance] = useState<Balance>();
 
-  useEffect(() => {
-    async function loadTransactions(): Promise<void> {
-      // TODO
-    }
-
-    loadTransactions();
-  }, []);
+  async function handleLoadTransactions(): Promise<void> {
+    const response = await api.get(`transactions`);
+    const t: Transaction[] = response.data.transactions;
+    setTransactions(t);
+    const b: Balance = response.data.balance;
+    setBalance(b);
+  }
 
   return (
     <>
       <Header />
-      <Container>
+      <Container onLoad={handleLoadTransactions}>
         <CardContainer>
           <Card>
             <header>
               <p>Entradas</p>
               <img src={income} alt="Income" />
             </header>
-            <h1 data-testid="balance-income">R$ 5.000,00</h1>
+            <h1 data-testid="balance-income">R$ {balance?.income}</h1>
           </Card>
           <Card>
             <header>
               <p>Saídas</p>
               <img src={outcome} alt="Outcome" />
             </header>
-            <h1 data-testid="balance-outcome">R$ 1.000,00</h1>
+            <h1 data-testid="balance-outcome">R$ {balance?.outcome}</h1>
           </Card>
           <Card total>
             <header>
               <p>Total</p>
               <img src={total} alt="Total" />
             </header>
-            <h1 data-testid="balance-total">R$ 4000,00</h1>
+            <h1 data-testid="balance-total">R$ {balance?.total}</h1>
           </Card>
         </CardContainer>
 
@@ -79,20 +76,18 @@ const Dashboard: React.FC = () => {
                 <th>Data</th>
               </tr>
             </thead>
-
             <tbody>
-              <tr>
-                <td className="title">Computer</td>
-                <td className="income">R$ 5.000,00</td>
-                <td>Sell</td>
-                <td>20/04/2020</td>
-              </tr>
-              <tr>
-                <td className="title">Website Hosting</td>
-                <td className="outcome">- R$ 1.000,00</td>
-                <td>Hosting</td>
-                <td>19/04/2020</td>
-              </tr>
+              {transactions.map((transaction) => (
+                <tr key={transaction.id}>
+                  <td className="title">{transaction.title}</td>
+                  <td className={transaction.type}>
+                    {transaction.type === 'outcome' && '- '}
+                    R$ {formatValue(transaction.value)}
+                  </td>
+                  <td>{transaction.category.title}</td>
+                  <td>{formatDate(transaction.created_at)}</td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </TableContainer>
--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,5 @@
-const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
-
+const formatValue = (value: number): string => {
+  Intl.NumberFormat().format(value);
+  return `${value},00`;
+};
 export default formatValue;
